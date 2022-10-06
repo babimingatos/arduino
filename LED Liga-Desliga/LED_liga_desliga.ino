0cccffa0f0aadc8aaa6814dd809ac9420f74171e
Barbara Mingatos <72284498+babimingatos@users.noreply.github.com>
2022-10-06 17:15:31 -0300

    Update LED_liga_desliga.ino

--- LED Liga-Desliga/LED_liga_desliga.ino
+++ LED Liga-Desliga/LED_liga_desliga.ino
@@ -1,10 +1,10 @@
 void setup() {
-pinMode(13,OUTPUT);
+  pinMode(13,OUTPUT);
 }
 
 void loop() {
-digitalWrite(13, HIGH); 
-delay(1000); 
-digitalWrite(13, LOW); 
-delay(1000); 
+  digitalWrite(13, HIGH); 
+  delay(1000); 
+  digitalWrite(13, LOW); 
+  delay(1000); 
 }
